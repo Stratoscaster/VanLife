b2a2a0d917eff83ef61e98419d162a7b2e7a18c2
Stratoscaster <stratoscaster191@gmail.com>
2025-06-26 17:15:04 -0700

    fixed width and padding in body/html/root

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,14 @@ import './App.css'
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import About from "./components/About.jsx";
 import Home from './components/Home.jsx';
-
+import { createGlobalStyle } from "styled-components"
+const GlobalStyle = createGlobalStyle`
+  body, .react-root {
+    padding: 0;
+    margin: 0;
+    min-width: 100%;
+  }
+`
 function App() {
 
 	return (
@@ -15,7 +22,7 @@ function App() {
 				<Route path={"/about"}
 					   element={<About/>}/>
 			</Routes>
-
+			<GlobalStyle/>
 		</BrowserRouter>
 
 	)
