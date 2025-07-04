6c0c86f5a699d1d9393e71e9221a83ba0497f67b
Stratoscaster <stratoscaster191@gmail.com>
2025-07-04 14:49:33 -0700

    Added Home page image and basic styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
 					</div>
 					<p>Our mission is to enliven your road trip with the perfect travel van rental. Our vans are
 						recertified before each trip to ensure your travel plans can go off without a hitch.
-
+						<br/><br/>
 						Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4
 						wheels.</p>
 					<div className={'ABOUT__explore-container'}>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import NavBar from "./NavBar/NavBar.jsx";
-
+import homeImage from '../assets/images/home-hero.png';
+import './Home.css';
 export default function Home() {
 	return (
-
-		<div>
-			<p>Home page</p>
+		<div className={'HOME__page-container'}>
+			<img className={'HOME__background-image'} src={homeImage}/>
 		</div>
 	);
 }
